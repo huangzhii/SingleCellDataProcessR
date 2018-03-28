83922907009adde62e275284f222a21707dd50df
Zhi Huang <huangzz@umich.edu>
2018-03-28 11:49:28 -0400

    first commit

---
+++ main.R
@@ -0,0 +1,5 @@
+# 03/28/2018 Zhi Huang
+# Cell Range R Kit https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/rkit
+# installation: just one line: source("http://cf.10xgenomics.com/supp/cell-exp/rkit-install-2.0.0.R")
+library(cellrangerRkit)
+packageVersion("cellrangerRkit")
